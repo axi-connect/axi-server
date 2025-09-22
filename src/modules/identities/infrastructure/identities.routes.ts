8096ad8150d712db9b8ed44fffca85e2ca803424
kodecol <gestion.proyectos.axi@gmail.com>
2025-09-22 10:31:23 -0500

    ♻️ identity refactoring

--- src/modules/identities/infrastructure/identities.routes.ts
+++ src/modules/identities/infrastructure/identities.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { CompaniesRouter } from "../../companies/infrastructure/companies.routes.js";
 import { UsersRouter } from "../../users/infrastructure/users.routes.js";
 import { AgentsRouter } from "../../agents/infrastructure/agents.routes.js";
+import { CompaniesRouter } from "../../companies/infrastructure/companies.routes.js";
 
 export const IdentitiesRouter = Router();
 
 // Mount sub-resources under /identities
-IdentitiesRouter.use('/companies', CompaniesRouter);
 IdentitiesRouter.use('/users', UsersRouter);
 IdentitiesRouter.use('/agents', AgentsRouter);
+IdentitiesRouter.use('/companies', CompaniesRouter);
